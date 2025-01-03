b9c75fe3f3d0f6d5c130c2925ae253022977efab
Leunsel <67030449+Leunsel@users.noreply.github.com>
2025-01-03 10:44:05 +0100

    Create Transform Base Address Pointer Hook.Settings.lua

---
+++ TemplateLoader/Templates/Transform Base Address Pointer Hook.Settings.lua
@@ -0,0 +1,10 @@
+local Settings = {
+    InSubMenu = true,
+    SubMenuName = "Auto-Assemble",
+    Caption = "Transform Base Address Pointer Hook",
+    Shortcut = "",
+    AskForInjectionAddress = true,
+    AskForHookName = true,
+    AppendToHookName = "Hook"
+}
+return Settings
