8d76c9c061fc73d70bd6754db2dc79189467f89f
Leunsel <67030449+Leunsel@users.noreply.github.com>
2025-01-21 15:27:13 +0100

    Update Module.Helper.lua
    Added comments

--- TableModules/Module.Helper.lua
+++ TableModules/Module.Helper.lua
@@ -1,49 +1,95 @@
 local NAME = 'CTI.FormManager'
-local AUTHOR = {'Leunsel', 'LeFiXER'}
+local AUTHOR = {'Leunsel', 'LeFiXER', 'TheyCallMeTim13'}
 local VERSION = '1.0.1'
 local DESCRIPTION = 'Cheat Table Interface (Helper)'
 
+--
+--- Would contain several configuration properties that can be customized.
+--- Set within the Table Lua Script and passed to the constructor of the class.
+--- None in this case.
+----------
 Helper = {}
+
+--
+--- Set the metatable for the Helper object so that it behaves as an "object-oriented class".
+----------
 Helper.__index = Helper
 
+--
+--- Constructor for the Helper class. Initializes default formatters and settings.
+--- @return A new instance of Helper.
+----------
 function Helper:new()
     local obj = setmetatable({}, self)
     return obj
 end
 
+--
+--- Returns the current process name.
+--- @return string: The name of the current process.
+----------
 function Helper:getProcess()
     return process
 end
 
+--
+--- Returns the current process name with the ".exe" extension removed (if present).
+--- @return string: The current process name without the ".exe" extension.
+----------
 function Helper:getProcessTrimmed()
     return process:gsub("%.exe$", "")
 end
 
+--
+--- Retrieves the first module in the list of modules loaded by the game.
+--- @return table|nil: The first module if available, otherwise nil.
+----------
 function Helper:getGameModule()
     local modules = enumModules()
     return (modules and modules[1]) or nil
 end
 
+--
+--- Checks whether the game's main module is 64-bit.
+--- @return boolean|nil: True if the module is 64-bit, otherwise nil.
+----------
 function Helper:getGameModuleIs64Bit()
     local gm = self:getGameModule()
     return gm and gm.Is64Bit or nil
 end
 
+--
+--- Retrieves the name of the game's main module.
+--- @return string|nil: The name of the main module, or nil if not available.
+----------
 function Helper:getGameModuleName()
     local gm = self:getGameModule()
     return gm and gm.Name or nil
 end
 
+--
+--- Retrieves the file path to the main module of the game.
+--- @return string|nil: The path to the main module file, or nil if not available.
+----------
 function Helper:getGameModulePathToFile()
     local gm = self:getGameModule()
     return gm and gm.PathToFile or nil
 end
 
+--
+--- Retrieves the address of the main module of the game.
+--- @return string|nil: The address of the main module, or nil if not available.
+----------
 function Helper:getGameModuleAddress()
     local gm = self:getGameModule()
     return gm and gm.Address or nil
 end
 
+--
+--- Retrieves detailed information about the game's main module, including its name, 64-bit status,
+--- file path, address, and version.
+--- @return table: A table containing the modules information.
+----------
 function Helper:getGameModuleInfo()
     return {
             name = self:getGameModuleName(),
@@ -54,6 +100,11 @@ function Helper:getGameModuleInfo()
         }
 end
 
+--
+--- Retrieves detailed string representations of the game's main module information, including its name,
+--- 64-bit status, file path, address, and version string.
+--- @return table: A table containing string representations of the module's details.
+----------
 function Helper:getGameModuleInfoStrs()
     return {
             name = self:getGameModuleName(),
@@ -64,15 +115,28 @@ function Helper:getGameModuleInfoStrs()
        }
 end
 
+--
+--- Retrieves the version of the game's main module.
+--- @return string|nil: The version of the main module, or nil if unavailable.
+----------
 function Helper:getGameVersion()
     local path = self:getGameModulePathToFile()
     return path and getFileVersion(path) or nil
 end
 
+--
+--- Retrieves the version string of the game's main module.
+--- @return string|nil: The version string, or nil if unavailable.
+----------
 function Helper:getGameVersionStr()
     return self:getFileVersionStr(self:getGameModulePathToFile())
 end
 
+--
+--- Retrieves the file version of a given path and returns it as a string.
+--- @param path: The file path.
+--- @return string|nil: The file version string, or nil if the version cannot be retrieved.
+----------
 function Helper:getFileVersionStr(path)
     if not path then return nil end
     local _, vt = getFileVersion(path)
@@ -82,10 +146,19 @@ function Helper:getFileVersionStr(path)
     return nil
 end
 
+--
+--- Returns a string representation of the registry size (x64 or x32) based on the game's main module's architecture.
+--- @return string: A string indicating whether the game is running in 64-bit or 32-bit mode.
+----------
 function Helper:getRegistrySizeStr()
     return self:getGameModuleIs64Bit() and "(x64)" or "(x32)"
 end
 
+--
+--- Prints the details of the address list, including the ID, description, and type of each address.
+--- If no address list is found, an error message is displayed.
+--- @return None.
+----------
 function Helper:printTableFeatures()
     local al = getAddressList()
     if not al then
@@ -97,6 +170,11 @@ function Helper:printTableFeatures()
     end
 end
 
+--
+--- Prints the details of auto-assembler scripts in the address list, including the ID and description.
+--- If no address list or auto-assembler scripts are found, an error message is displayed.
+--- @return None.
+----------
 function Helper:printTableScripts()
     local al = getAddressList()
     if not al then
@@ -110,6 +188,11 @@ function Helper:printTableScripts()
     end
 end
 
+--
+--- Prints detailed information about each module, including the module's name, address, and file path.
+--- The architecture (x64 or x32) is also displayed for each module.
+--- @return None.
+----------
 function Helper:printModuleDetails()
     local ind = '  '
     local modules = enumModules()
