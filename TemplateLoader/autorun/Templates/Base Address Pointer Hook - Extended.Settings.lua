ad1da85252a079158d1cb3601f322579d88a7250
Leunsel <67030449+Leunsel@users.noreply.github.com>
2025-01-26 17:47:25 +0100

    Add files via upload

---
+++ TemplateLoader/autorun/Templates/Base Address Pointer Hook - Extended.Settings.lua
@@ -0,0 +1,10 @@
+local Settings = {
+    InSubMenu = true,
+    SubMenuName = "Auto-Assemble",
+    Caption = "Base Address Pointer Hook - Extended",
+    Shortcut = "",
+    AskForInjectionAddress = true,
+    AskForHookName = true,
+    AppendToHookName = "Hook"
+}
+return Settings
---
+++ TemplateLoader/autorun/Templates/Lua Write.Settings.lua
@@ -0,0 +1,10 @@
+local Settings = {
+    InSubMenu = true,
+    SubMenuName = "Auto-Assemble",
+    Caption = "Lua Write",
+    Shortcut = "",
+    AskForInjectionAddress = true,
+    AskForHookName = true,
+    AppendToHookName = "Hook"
+}
+return Settings
